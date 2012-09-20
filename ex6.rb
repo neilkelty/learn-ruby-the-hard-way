53509a579f4642000e5e932ea9f4b9a0fa0aee79
Neil Kelty <neil@neilkelty.com>
2012-09-20 01:00:43 -0400

    Complete Exercise 6

---
+++ ex6.rb
@@ -0,0 +1,22 @@
+# Exercise 6: Strings And Text
+
+x = "There are #{10} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}"
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This string is the left side of..."
+e = "a string with the right side."
+
+puts w + e
