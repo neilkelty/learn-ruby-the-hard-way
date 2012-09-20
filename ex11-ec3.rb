31b9f6916ba1cd925846f5d7db1443c7add252fb
Neil Kelty <neil@neilkelty.com>
2012-09-20 08:42:11 -0400

    Complete Exercise 11

---
+++ ex11-ec3.rb
@@ -0,0 +1,8 @@
+print "Which NFL team is your favorite? "
+favorite_nfl_team = gets.chomp()
+print "Who is your favorite MLB player? "
+favorite_mlb_player = gets.chomp()
+print "How many #{favorite_nfl_team} games have you been to this season? "
+num_games_attended = gets.chomp()
+
+puts "So, your favorite NFL team is the #{favorite_nfl_team} and you've attended #{num_games_attended} of their games this season. Also, your favorite MLB player is #{favorite_mlb_player}."
---
+++ ex11.rb
@@ -0,0 +1,10 @@
+# Exercise 11: Asking Questions
+
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
