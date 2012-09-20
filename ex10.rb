f628f1dde14b582992e78a8de178c3a2eb8a0e6b
Neil Kelty <neil@neilkelty.com>
2012-09-20 01:30:19 -0400

    Complete Exercise 10.

---
+++ ex10.rb
@@ -0,0 +1,17 @@
+# Exercise 10: What Was That?
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backlash_cat = "I'm \\ a \\ cat."
+
+fat_cat = <<MY_HEREDOC
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+MY_HEREDOC
+
+puts tabby_cat
+puts persian_cat
+puts backlash_cat
+puts fat_cat
