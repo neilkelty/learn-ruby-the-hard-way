527662c1f37ee6c07c5c1c439bafb2548076dc7d
Neil Kelty <neil@neilkelty.com>
2013-03-09 14:34:17 -0500

    Prepare Exercise 37.

---
+++ ex37.rb
@@ -0,0 +1,277 @@
+# ========================================
+# Keywords
+# ========================================
+
+# alias
+
+
+# and
+
+
+# BEGIN
+
+
+# begin
+
+
+# break
+
+
+# case
+
+
+# class
+
+
+# def
+
+
+# defined?
+
+
+# do
+
+
+# else
+
+
+# elsif
+
+
+# END
+
+
+# end
+
+
+# ensure
+
+
+# false
+
+
+# for
+
+
+# if
+
+
+# in
+
+
+# module
+
+
+# next
+
+
+# nil
+
+
+# not
+
+
+# or
+
+
+# redo
+
+
+# rescue
+
+
+# retry
+
+
+# return
+
+
+# self
+
+
+# super
+
+
+# then
+
+
+# true
+
+
+# undef
+
+
+# unless
+
+
+# until
+
+
+# when
+
+
+# while
+
+
+# yield
+
+
+# ========================================
+# Data Types
+# For data types, write out what makes up each one. For example, with strings write out how you create a string. For numbers write out a few numbers.
+# ========================================
+
+# true
+
+
+# false
+
+
+# nil
+
+
+# constants
+
+
+# strings
+
+
+# numbers
+
+
+# ranges
+
+
+# arrays
+
+
+# hashes
+
+# ========================================
+# String Escapes Sequences 
+# For string escape sequences, use them in strings to make sure they do what you think they do.
+# ========================================
+
+# \\
+
+
+# \'
+
+
+# \"
+
+
+# \a
+
+
+# \b
+
+
+# \f
+
+
+# \n
+
+
+# \r
+
+
+# \t
+
+
+# \v
+
+
+# ========================================
+# Operators 
+# Some of these may be unfamiliar to you, but look them up anyway. Find out what they do, and if you still can't figure it out, save it for later.
+# ========================================
+
+# ::
+
+
+# []
+
+
+# **
+
+
+# -(unary)
+
+
+# +(unary)
+
+
+# !
+
+
+# ~
+
+
+# *
+
+
+# /
+
+
+# %
+
+
+# +
+
+
+# -
+
+
+# <<
+
+
+# >>
+
+
+# &
+
+
+# |
+
+
+# >
+
+
+# >=
+
+
+# <
+
+
+# <=
+
+
+# <=>
+
+
+# ==
+
+
+# ===
+
+
+# !=
+
+# =~
+
+
+# !~
+
+
+# &&
+
+
+# ||
+
+
+# ..
+
+
+# ...
+
