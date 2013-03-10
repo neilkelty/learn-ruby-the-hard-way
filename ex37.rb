617df84d29d903aa02792d7a1e19408332580c15
Neil Kelty <neil@neilkelty.com>
2013-03-09 19:41:01 -0500

    Complete several symbols in Exercise 37.

--- ex37.rb
+++ ex37.rb
@@ -1,120 +1,156 @@
+def print_separator(symbol_name, activated=false)
+  if activated
+    puts ""
+    puts "="*10 + " #{symbol_name} " + "="*10
+  end
+end
+
 # ========================================
 # Keywords
 # ========================================
 
 # alias
-
+print_separator("alias")
 
 # and
+print_separator("and", true)
+
+if 3 == 3 and (true or false)
+  puts "Go wild and crazy for the 'and' statement!"
+else
+  puts "Just go home!"
+end
 
 
 # BEGIN
-
+print_separator("BEGIN")
 
 # begin
+print_separator("begin")
 
 
 # break
+print_separator("break")
 
 
 # case
+print_separator("case")
 
 
 # class
+print_separator("class")
 
 
 # def
+print_separator("def", true)
+
+def methods_are_really_fun
+  puts nil
+end
+
+methods_are_really_fun
 
 
 # defined?
+print_separator("defined?")
 
 
 # do
+print_separator("do")
 
 
 # else
+print_separator("else")
+
 
 
 # elsif
+print_separator("")
 
 
 # END
+print_separator("")
 
 
 # end
+print_separator("")
 
 
 # ensure
+print_separator("")
 
 
 # false
+print_separator("")
 
 
 # for
+print_separator("")
 
 
 # if
+print_separator("")
 
 
 # in
-
+print_separator("")
 
 # module
-
+print_separator("")
 
 # next
-
+print_separator("")
 
 # nil
-
+print_separator("")
 
 # not
-
+print_separator("")
 
 # or
-
+print_separator("")
 
 # redo
-
+print_separator("")
 
 # rescue
-
+print_separator("")
 
 # retry
-
+print_separator("")
 
 # return
-
+print_separator("")
 
 # self
-
+print_separator("")
 
 # super
-
+print_separator("")
 
 # then
-
+print_separator("")
 
 # true
-
+print_separator("")
 
 # undef
-
+print_separator("")
 
 # unless
-
+print_separator("")
 
 # until
-
+print_separator("")
 
 # when
-
+print_separator("")
 
 # while
-
+print_separator("")
 
 # yield
-
+print_separator("")
 
 # ========================================
 # Data Types
@@ -122,30 +158,31 @@
 # ========================================
 
 # true
-
+print_separator("")
 
 # false
-
+print_separator("")
 
 # nil
-
+print_separator("")
 
 # constants
-
+print_separator("")
 
 # strings
-
+print_separator("")
 
 # numbers
-
+print_separator("")
 
 # ranges
-
+print_separator("")
 
 # arrays
-
+print_separator("")
 
 # hashes
+print_separator("")
 
 # ========================================
 # String Escapes Sequences 
@@ -153,34 +190,34 @@
 # ========================================
 
 # \\
-
+print_separator("")
 
 # \'
-
+print_separator("")
 
 # \"
-
+print_separator("")
 
 # \a
-
+print_separator("")
 
 # \b
-
+print_separator("")
 
 # \f
-
+print_separator("")
 
 # \n
-
+print_separator("")
 
 # \r
-
+print_separator("")
 
 # \t
-
+print_separator("")
 
 # \v
-
+print_separator("")
 
 # ========================================
 # Operators 
@@ -188,90 +225,99 @@
 # ========================================
 
 # ::
-
+print_separator("")
 
 # []
-
+print_separator("")
 
 # **
-
+print_separator("")
 
 # -(unary)
-
+print_separator("")
 
 # +(unary)
-
+print_separator("+(unary)")
 
 # !
-
+print_separator("!")
 
 # ~
-
+print_separator("~")
 
 # *
-
+print_separator("*")
+puts 5 * 4
 
 # /
-
+print_separator("")
 
 # %
-
+print_separator("")
 
 # +
-
+print_separator("")
 
 # -
-
+print_separator("")
 
 # <<
-
+print_separator("")
 
 # >>
-
+print_separator("")
 
 # &
-
+print_separator("")
 
 # |
-
+print_separator("")
 
 # >
-
+print_separator("")
 
 # >=
-
+print_separator("")
 
 # <
-
+print_separator("")
 
 # <=
-
+print_separator("")
 
 # <=>
-
+print_separator("")
 
 # ==
-
+print_separator("")
 
 # ===
-
+print_separator("")
 
 # !=
+print_separator("")
+
 
 # =~
+print_separator("")
 
 
 # !~
+print_separator("")
 
 
 # &&
+print_separator("")
 
 
 # ||
-
+print_separator("")
 
 # ..
+print_separator("")
 
 
 # ...
+print_separator("")
+
 
