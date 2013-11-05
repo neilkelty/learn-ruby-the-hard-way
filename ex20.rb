c25c3d36f60c0e7edb7765862938a45f132bd371
Neil Kelty <neil@neilkelty.com>
2013-11-05 06:08:39 -0500

    Complete Exercise 20

--- ex20.rb
+++ ex20.rb
@@ -1,3 +1,40 @@
-# Title:
-# URL: http://ruby.learncodethehardway.org/book/ex.html
+# Title: Exercise 20: Functions And Files
+# URL: http://ruby.learncodethehardway.org/book/ex20.html
 
+input_file = ARGV[0]
+
+def print_all(f)
+  puts f.read()
+end
+
+def rewind(f)
+  f.seek(0, IO::SEEK_SET)
+end
+
+def print_a_line(line_count, f)
+  puts "#{line_count} #{f.readline()}"
+end
+
+current_file = File.open(input_file)
+
+puts "First let's print the whole file:"
+puts # a blank line
+
+print_all(current_file)
+puts
+
+puts "Now let's rewind, kind of like a tape."
+
+rewind(current_file)
+puts
+
+puts "Let's print three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
