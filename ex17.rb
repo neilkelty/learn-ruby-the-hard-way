cbc8fa4d2eb64eceefbf4d411b6d7871307c7aa5
Neil Kelty <neil@neilkelty.com>
2013-11-05 05:37:45 -0500

    Complete Exercise 17

--- ex17.rb
+++ ex17.rb
@@ -1,3 +1,27 @@
-# Title:
-# URL: http://ruby.learncodethehardway.org/book/ex.html
+# Title: Exercise 17: More Files
+# URL: http://ruby.learncodethehardway.org/book/ex17.html
 
+from_file, to_file = ARGV
+script = $0
+
+puts "Copying from #{from_file} to #{to_file}"
+
+# we could do these two on line line too, how?
+input = File.open(from_file)
+indata = input.read()
+
+# indata = File.open(from_file).read()
+
+puts "The input file is #{indata.length} bytes long."
+
+puts "Does the output file exist? #{File.exists? to_file}"
+puts "Read, hit RETURN to continue, CTRL-C to abort."
+STDIN.gets
+
+output = File.open(to_file, 'w')
+output.write(indata)
+
+puts "Alright, all done."
+
+output.close
+input.close
