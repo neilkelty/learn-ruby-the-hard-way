0b18a1f7e5af973a658dc357db943875747fe1c4
Neil Kelty <neil@neilkelty.com>
2013-11-05 05:43:32 -0500

    Complete Exercise 18

--- ex18.rb
+++ ex18.rb
@@ -1,3 +1,28 @@
-# Title:
-# URL: http://ruby.learncodethehardway.org/book/ex.html
+# Title: Exercise 18: Names, Variables, Code, Functions
+# URL: http://ruby.learncodethehardway.org/book/ex18.html
 
+# this one is like your scripts with argv
+def puts_two(*args)
+  arg1, arg2 = args
+  puts "arg1: #{arg1}, arg2 #{arg2}"
+end
+
+# ok, that *args is actually pointless, we can just do this
+def puts_two_again(arg1, arg2)
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+# this just take one argument
+def puts_one(arg1)
+  puts "arg1: #{arg1}"
+end
+
+# this one takes no arguments
+def puts_none()
+  puts "I got nothin'."
+end
+
+puts_two("Zed", "Shaw")
+puts_two_again("Zed", "Shaw")
+puts_one("First!")
+puts_none()
