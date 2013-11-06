a7380d14c5a7c1c13fb6bded1db244be9ebd7293
Neil Kelty <neil@neilkelty.com>
2013-11-06 06:15:09 -0500

    Complete Exercise 25

--- ex25.rb
+++ ex25.rb
@@ -1,3 +1,46 @@
-# Title:
-# URL: http://ruby.learncodethehardway.org/book/ex.html
+# Title: Exercise 25: Even More Practice
+# URL: http://ruby.learncodethehardway.org/book/ex25.html
 
+module Ex25
+  def self.break_words(stuff)
+    # This function will break up words for us.
+    words = stuff.split(' ')
+    words
+  end
+
+  def self.sort_words(words)
+    # Sort the words
+    words.sort
+  end
+
+  def self.print_first_word(words)
+    # Prints the firs word and shufts the others down by one.
+    word = words.shift
+    puts word
+  end
+
+  def self.print_last_word(words)
+    word = words.pop
+    puts word
+  end
+
+  def self.sort_sentence(sentence)
+    # Takes in a full sentence and returns the sorted words.
+    words = break_words(sentence)
+    sort_words(words)
+  end
+
+  def self.print_first_and_last(sentence)
+    # Prints the first and last words of a sentence
+    words = break_words(sentence)    
+    print_first_word(words)
+    print_last_word(words)
+  end
+
+  def self.print_first_and_last_sorted(sentence)
+    # Sorts the words then prints the first and last one.
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+  end
+end
